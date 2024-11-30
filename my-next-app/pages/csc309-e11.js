4af583149dd06afa7399bde3dfd7bc3de9809154
kimlan <kimlan.huynh@mail.utoronto.ca>
2024-11-30 07:12:02 -0500

    all

---
+++ my-next-app/pages/csc309-e11.js
@@ -0,0 +1,8 @@
+// pages/csc309-e11.js
+export default function Tester() {
+    return (
+        <div id="auto-tester">
+            Hi. I am huynhki6, and I have learned how to develop a full-stack web app and deploy it!
+        </div>
+    );
+}
